468083660bbd03d4fba69a928f9be753620af083
Winkas09 <edvinas17935@gmail.com>
2024-09-04 22:48:38 +0300

    done ?

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <nav className="App-nav">
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Product</a>
+          </li>
+          <li>
+            <a href="#">Company</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </nav>
+      <section className="App-content">
+        <img className="App-content-image" src="https://via.placeholder.com/200" alt="placeholder" />
+        <div className="App-content-cards">
+          <div className="App-content-card">
+            <h2>About</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus sit amet urna tincidunt efficitur. Nulla facilisi.</p>
+          </div>
+          <div className="App-content-card">
+            <h2>Company</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus sit amet urna tincidunt efficitur. Nulla facilisi.</p>
+          </div>
+          <div className="App-content-card">
+            <h2>Services</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus sit amet urna tincidunt efficitur. Nulla facilisi.</p>
+          </div>
+        </div>
+      </section>
+      <footer className="App-footer">
+        <div className="App-footer-content">
+          <h2>Content</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus sit amet urna tincidunt efficitur. Nulla facilisi.</p>
+          <h3>Sub header</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus sit amet urna tincidunt efficitur. Nulla facilisi.</p>
+        </div>
+        <div className="footer-nav">
+          <h2>Navigation</h2>
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">Product</a>
+            </li>
+            <li>
+              <a href="#">Company</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </footer>
     </div>
   );
 }
